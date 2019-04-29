49487eaf5ed8a76229007ab4b73a2c2b2e29533a
Venkata <venkata@yombu.com>
2019-04-28 21:13:07 -0400

    V0.0.1: Completed writing unit tests for operations

--- operation_test.go
+++ operation_test.go
@@ -1,3 +1,66 @@
 package RBAC
 
+import (
+	"github.com/flannel-dev-lab/RBAC/database"
+	"log"
+	"os"
+	"testing"
+)
+
+// RBAC Object test parameters
+var operationObject OperationObject
+
+func setupOperationObjectTest() {
+	dbService, err := database.CreateDatabaseObject("mysql")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = dbService.CreateDBConnection(
+		os.Getenv("RBAC_DB_DRIVER"),
+		os.Getenv("RBAC_DB_USERNAME"),
+		os.Getenv("RBAC_DB_PASSWORD"),
+		os.Getenv("RBAC_DB_HOSTNAME"),
+		os.Getenv("RBAC_DB_NAME"),
+		os.Getenv("RBAC_DB_PORT"))
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	operationObject.DBService = dbService
+}
+
+func tearDownOperationObjectTest() {
+	err := operationObject.DBService.CloseConnection()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func TestAddOperation(t *testing.T) {
+	setupOperationObjectTest()
+	object, err := operationObject.AddOperation("test-operation", "test-operation-description")
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Cleanup
+	_, err = operationObject.DeleteOperation(object.Id)
+	tearDownOperationObjectTest()
+}
+
+func TestDeleteOperation(t *testing.T) {
+	setupOperationObjectTest()
+	object, err := operationObject.AddOperation("test-operation", "test-operation-description")
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// Cleanup
+	_, err = operationObject.DeleteOperation(object.Id)
+	tearDownOperationObjectTest()
+}
 
